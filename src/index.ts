2ba3c3d7fb2cca67532511774a3792d3bd1e2f8b
tharun01111 <tharunadhi33@gmail.com>
2026-05-25 12:59:29 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+
+app.get('/', (req: any, res: any) => {
+    res.send('Hello from express server');
+});
+
+app.listen(PORT, () => {
+    console.log('Server is running on http://localhost:' + PORT);
+});
+
